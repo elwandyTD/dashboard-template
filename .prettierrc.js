83abc381ccef89c1be4f78d303c8a11ffba3aeb8
elwanditirtana1945a <elwanditirtana1945a@gmail.com>
2023-10-25 12:44:39 +0900

    add editorconfig and prettier

---
+++ .prettierrc.js
@@ -0,0 +1,12 @@
+/** @type {import("prettier").Config} */
+const config = {
+	parser: "babel-ts",
+	useTabs: true,
+	tabWidth: 2,
+	semi: true,
+	singleQuote: false,
+	jsxSingleQuote: false,
+	arrowParens: "avoid",
+};
+
+module.exports = config;
